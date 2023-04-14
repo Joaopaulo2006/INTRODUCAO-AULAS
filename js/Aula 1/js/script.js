e0056caaa52f2271fc50af0b9afcd1ac0640a134
Joaopaulo2006 <125692510+Joaopaulo2006@users.noreply.github.com>
2023-04-14 12:55:52 -0300

    Add files via upload

---
+++ js/Aula 1/js/script.js
@@ -0,0 +1,3 @@
+function myHello(){
+alert("Ola Mundo!");
+}
---
+++ js/Aula 2/js/script.js
@@ -0,0 +1,42 @@
+function myHello(){
+
+    var iptMsg = document.getElementById("Wever");
+    var msg = Wever.value;
+    var spn = document.getElementById("spnMsg");
+
+    spn.innerHTML = msg;
+    spn.style.color = "";
+}
+function msApaga(){
+
+
+    var spn = document.getElementById("spnMsg");
+
+    spn.innerHTML = "";
+
+
+}
+function msColor(){
+
+    var spn = document.getElementById("spnMsg");
+
+    spn.style.color = "red";
+
+
+}
+function msColor2(){
+
+    var spn = document.getElementById("spnMsg");
+
+    spn.style.color = "";
+
+
+}
+function msg() {
+
+    var mensg = document.getElementById("Wever");
+    var msg = Wever.value;
+
+    spn =mensg.ariaValueText ;
+}
+
