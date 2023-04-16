9a43eb6678bfc51eee2e6f493a4583439b85fafb
Joaopaulo2006 <jpo54545@gmail.com>
2023-04-16 01:22:40 -0300

    atualização

--- Projeto trabalho 2/js/script.js
+++ Projeto trabalho 2/js/script.js
@@ -1,8 +1,33 @@
-function nome() {
+// function nome() {
+//     const nom = document.getElementById("jogador").value;
+//     var n = nom
+//     document.getElementById("jogador").textContent = "Jogador: "+ n;
+// }
+// function pegaLetra(){
+//     var ID = $(this).attr('data-id');
+// likeBtn.addEventListener('click', () => {
+//     console.log(dataId);
+// })}
+//     console.log(ID);
+// const likeBtn = document.querySelector('.botaolike')
+// document.getElementById("bancoLetras").textContent = banco + dataId;
+// let banco = $('#bancoLetras').val();
+// $("#bancoLetras").html(banco + dataId);
+function handleClick(button) {
+    let dataId = button.getAttribute('data-id');
+    $("#pegaLetra").html(dataId);
+    var pegaLetra = document.getElementById("pegaLetra");
+    $("#bancoLetra").html(dataId + pegaLetra);
+    var banco = document.getElementById("bancoLetra");
+    let junta = banco + pegaLetra;
+    const texto = pegaLetra.textContent;
+    // $("#bancoLetra").val(banco + pegaLetra);
 
-    const nom = document.getElementById("jogador").value;
+    junta.innerHTML = ("Letras: " + pegaLetra);
 
-    var n = nom
-    document.getElementById("jogador").textContent = "Jogador: "+ n;
-
+    // var txt = "";
+    // for(let k = 0; k < banco ; k++){
+    //     txt = txt + banco;
+    // }
+    console.log(banco);
 }
--- js/Aula 4/js/script.js
+++ js/Aula 4/js/script.js
@@ -3,14 +3,14 @@ function Rest(){
     const mensagem = document.getElementById("msg").value ;
     const n = document.getElementById("nmsg").value ;
     const containerMsg = document.getElementById("rspmsg");
-    
+
     var txt = "";
 
-    for(let k = 0; k<n; k++){
+    for(let i = 0; i<n; i++){
         console.log(mensagem);
         txt = txt + mensagem + "<br>";
     }
     containerMsg.innerHTML = txt;
-    
+
 
 }
